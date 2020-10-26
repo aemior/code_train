e61f9510d64c2ac09d99765574373255b747459c
aemior <1120132531@bit.edu.cn>
2020-10-26 23:36:40 +0800

    fin 200601

--- 200601/main.cpp
+++ 200601/main.cpp
@@ -2,58 +2,68 @@
 
 using namespace std;
 
+struct samp{
+    int x;
+    int y;
+    string lb;
+};
+
 int main()
 {
     freopen("test.txt", "r", stdin);
     int n,m;
     cin >> n >> m;
-    vector<vector<int> > A,B;
+    vector<samp> test_set;
     for(int i=0; i<n; i++){
-        int x,y;
-        string cls;
-        cin >> x >> y >> cls;
-        if(cls == "A") A.push_back({x,y});
-        else B.push_back({x,y});
+        samp s1;
+        cin >> s1.x >> s1.y >> s1.lb;
+        test_set.push_back(s1);
     }
     for(int i=0;i<m;i++){
         int t0,t1,t2;
-        bool a_g,pflg = false;
+        bool a_g=false,pflg = false;
         cin >> t0 >> t1 >> t2;
-        if(t1*A[0][0]+t2*A[0][1]+t0 > 0) a_g = true;
-        else a_g = false;
-        for(int k=1; k<A.size(); k++){
-            if(a_g){
-                if(t1*A[k][0]+t2*A[k][1]+t0 <= 0){
-                    cout << "No" << endl;
-                    pflg = true;
-                    break;
-                }
-            }
-            else{
-                if(t1*A[k][0]+t2*A[k][1]+t0 >= 0){
-                    cout << "No" << endl;
-                    pflg = true;
-                    break;
-                }
-
-            }
+        if(test_set[0].x*t1+test_set[0].y*t2+t0 > 0){
+            if(test_set[0].lb == "A") a_g = true;
         }
-        if(pflg) continue;
-        for(int k=1; k<B.size(); k++){
-            if(a_g){
-                if(t1*B[k][0]+t2*B[k][1]+t0 >= 0){
+        else if(test_set[0].x*t1+test_set[0].y*t2+t0 < 0){
+            if(test_set[0].lb == "B") a_g = true;
+        }
+        else{
+            cout << "No" << endl;
+            continue;
+        }
+        for(int j=1;j<test_set.size(); j++){
+            int tmp = test_set[j].x*t1 + test_set[j].y*t2 + t0;
+            if(tmp == 0){
+                cout << "No" << endl;
+                pflg = true;
+            }
+            if(test_set[j].lb == "A"){
+                if(a_g){
+                    if(tmp < 0) pflg = true;
+                }
+                else{
+                    if(tmp > 0) pflg = true;
+                }
+                if(pflg){
                     cout << "No" << endl;
-                    pflg = true;
                     break;
                 }
             }
-            else{
-                if(t1*B[k][0]+t2*B[k][1]+t0 <= 0){
+            if(test_set[j].lb == "B"){
+                if(a_g){
+                    if(tmp > 0) pflg = true;
+                }
+                else{
+                    if(tmp < 0) pflg = true;
+                }
+                if(pflg){
                     cout << "No" << endl;
-                    pflg = true;
                     break;
                 }
             }
+
         }
         if(pflg) continue;
         cout << "Yes" << endl;
