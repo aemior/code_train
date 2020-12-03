01998fcd40215ec22ec3ff6168241f50b942eaff
Ming <snowshow4@gmail.com>
2020-12-03 22:07:33 +0800

    1203_2

---
+++ 180902/main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    freopen("test.txt", "r", stdin);
+    int n,s,t,pi=0,pj=0;
+    cin >> n;
+    vector<vector<int> > H;
+    vector<vector<int> > W;
+    for(int i=0; i<n; i++){
+        cin >> s >> t;
+        vector<int> vt;
+        vt.push_back(s);
+        vt.push_back(t);
+        H.push_back(vt);
+    }
+    for(int i=0; i<n; i++){
+        cin >> s >> t;
+        vector<int> vt;
+        vt.push_back(s);
+        vt.push_back(t);
+        W.push_back(vt);
+    }
+    long long T=0;
+    for(;pi <n;){
+        if(pj>=n) break;
+        for(;pj<n;){
+
+            if(W[pj][1] < H[pi][0]){
+                pj++;
+                continue;
+            }
+            else if(W[pj][0] > H[pi][1]){
+                pi++;
+                break;
+            }
+            else{
+                vector<int> tp;
+                tp.push_back(W[pj][0]);
+                tp.push_back(W[pj][1]);
+                tp.push_back(H[pi][0]);
+                tp.push_back(H[pi][1]);
+                sort(tp.begin(), tp.end());
+                T = T + (tp[2]-tp[1]);
+                pj++;
+            }
+        }
+    }
+    cout << T << endl;
+    return 0;
+}
