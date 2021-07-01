c68bfe6d3727c722fd1b67e5c1d179b10b0811cc
snowshow4@gmail.com <snowshow4@gmail.com>
2021-07-01 11:26:37 +0800

    mooc_002_ac

--- MOOC_ALG/mooc_002/main.cpp
+++ MOOC_ALG/mooc_002/main.cpp
@@ -5,100 +5,98 @@
 
 using namespace std;
 
-map<int, string> moves = {
-{1,"ABDE"},
-{2,"ABC"},
-{3,"BCEF"},
-{4,"ADG"},
-{5,"BDEFH"},
-{6,"CFI"},
-{7,"DEGH"},
-{8,"GHI"},
-{9,"EFHI"}
+const char *moves[9] = {
+"ABDE",
+"ABC",
+"BCEF",
+"ADG",
+"BDEFH",
+"CFI",
+"DEGH",
+"GHI",
+"EFHI"
 };
+
 
 int oriClocks[9];
 int Clocks[9];
+int res[9] = {5,5,5,5,5,5,5,5,5};
+
+class enmMove{
+	public:
+		int M[9];
+		enmMove(){
+			for (int i=0; i<9; ++i) {
+				M[i] = 0;
+			}
+		}
+		bool n(){
+			int op = 1;
+			int tc;
+			for (int i=0; i<9; ++i) {
+				tc = M[i] + op;
+				if (tc > 3) {
+					op = 1;
+					M[i] = 0;
+				}
+				else {
+					M[i] = tc;
+					return true;
+				}
+			}
+			if (op) {
+				return false;
+			}
+			return true;
+		}
+};
+
 
 void triClock(int i){
 	if(Clocks[i] == 3) Clocks[i] = 0;
 	else ++Clocks[i];
 }
 
-void moveClocks(const int *res, int len){
-	for(int i=0; i<len; ++i){
-		for(int j=0; j< moves[res[i]+1].length(); ++j){
-			triClock(moves[res[i]+1][j] - 'A');
-		}
-	}
-}
-
-bool isOk(){
+void moveClocks(const int *res){
 	for(int i=0; i<9; ++i){
-		if(Clocks[i]) return false;
+		for(int j=0; j<res[i]; ++j){
+			for(int k=0; moves[i][k]; ++k)
+				triClock(moves[i][k] - 'A');
+		}
 	}
-	return true;
 }
 
-bool okToUse(const int *res, int target, int len){
-	int cnt = 0;
-	for(int i=0; i<len; i++){
-		if(res[i] == target){
-			++cnt;
+void setRes(const enmMove &ms){
+	memcpy(Clocks, oriClocks, sizeof(oriClocks));
+	moveClocks(ms.M);
+	int CNT = 0;
+	int newCNT = 0;
+	for (int i=0; i<9; ++i) {
+		CNT += res[i];
+		newCNT += ms.M[i];
+		if (Clocks[i]) {
+			return;
 		}
-		if(cnt > 3) return false;
 	}
-	return true;
-}
-
-bool enmOP(int* & res,int curr, int limit){
-	int k;
-	if(curr > 0){
-		k=res[curr-1];
-	}
-	else{
-		k=0;
-	}
-	if(curr == limit-1){
-		for(; k<10; k++){
-			//if(okToUse(res, k, curr)) continue;
-			res[curr] = k;
-			memcpy(Clocks, oriClocks, sizeof(oriClocks));
-			moveClocks(res, limit);
-			if(isOk()) return true;
-		}
-		return false;
-	}
-	else{
-		for(;k<10;++k){
-			res[curr] = k;
-			if(enmOP(res, curr+1, limit)){
-				return true;
-			}
-		}
-		return false;
+	if (newCNT < CNT) {
+		memcpy(res, ms.M, sizeof(res));
 	}
 }
 
 int main(){
-	freopen("test.txt", "r", stdin);
+	//freopen("test.txt", "r", stdin);
+	//freopen("out.txt", "w", stdout);
 	for(int i=0; i<9; ++i){
 		cin >> oriClocks[i];
 	}
-	memcpy(Clocks, oriClocks, sizeof(oriClocks));
-	if(isOk()) return 0;
-	for(int i=1; i<37; i++){
-		int *res = new int[i];
-		if(enmOP(res, 0, i)){
-			for(int j=0; j<i; ++j){
-				cout << res[j]+1;
-				if(j<(i-1)) cout << ' ';
-			}
-			cout << endl;
-			delete []res;
-			break;
+	enmMove e;
+	do {
+		setRes(e);
+	} while (e.n());
+	for (int i=0; i<9; ++i) {
+		for (int j=0; j<res[i]; j++) {
+			cout << i+1 << ' ';
 		}
-		delete []res;
 	}
 	return 0;
 }
