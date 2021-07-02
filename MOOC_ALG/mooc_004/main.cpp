59fbc1701e5d318416c8ce70c78009e56ea53a4c
snowshow4@gmail.com <snowshow4@gmail.com>
2021-07-02 16:05:09 +0800

    Week 6

--- MOOC_ALG/mooc_004/main.cpp
+++ MOOC_ALG/mooc_004/main.cpp
@@ -3,23 +3,22 @@
 using namespace std;
 
 void int2bin(int i){
-	if (i==2) {
+	if (i == 2) {
 		cout << '2';
-	} else if (i == 1) {
-		cout << "2(0)";
+	} else if (i == 0) {
+		cout << '0';
 	}
 	else {
 		bool pflg = false;
-		for (int j=16; j>0; --j) {
+		for (int j=16; j>=0; --j) {
 			if ((i >> j) & 1) {
 				if (pflg) {
 					cout <<  '+';
 				}
-				if (j>2) {
-					cout << "2(";
-				}
-				int2bin(j);
-				if (j>2) {
+				cout <<  '2';
+				if (j != 1) {
+					cout << "(";
+					int2bin(j);
 					cout << ')';
 				}
 				pflg = true;
@@ -32,5 +31,6 @@ int main(){
 	int I;
 	cin >> I;
 	int2bin(I);
+	cout << endl;
 	return 0;
 }
