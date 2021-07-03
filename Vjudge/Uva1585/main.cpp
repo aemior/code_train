ab3d263d63b26cc92e0299de2f078066474c8018
snowshow4@gmail.com <snowshow4@gmail.com>
2021-07-03 14:49:47 +0800

    uva1585

---
+++ Vjudge/Uva1585/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main () {
+	freopen("test.txt", "r", stdin);
+	int N;
+	cin >> N;
+	cin.get();
+	while (N--)
+	{
+		char buf[90];
+		cin.getline(buf, 85);
+		int res = 0, cnt = 0;
+		for (int i=0; buf[i]; ++i) {
+			if (buf[i] == 'O') {
+				res += ++cnt;
+			} else if (buf[i] == 'X') {
+				cnt = 0;
+			}
+		}
+		cout << res << endl;
+	}
+	return 0;
+}
