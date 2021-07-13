41cd40deb5628fc66bf05cd7f3d1ae31723a0fe1
snowshow4@gmail.com <snowshow4@gmail.com>
2021-07-13 22:01:06 +0800

    more mooc code

---
+++ MOOC_CPP/mooc_008/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+class Complex {
+private:
+    double r,i;
+public:
+    void Print() {
+        cout << r << "+" << i << "i" << endl;
+    }
+	void operator=(const string &s) {
+		sscanf(s.c_str(), "%lf+%lfi", &r, &i);
+	}
+// 在此处补充你的代码
+};
+int main() {
+    Complex a;
+    a = "3+4i"; a.Print();
+    a = "5+6i"; a.Print();
+    return 0;
+}
---
+++ MOOC_CPP/mooc_12/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct A
+{
+	int v;
+	A(int vv):v(vv) { }
+	const A * getPointer() const {
+		return this;
+	}
+// 在此处补充你的代码
+};
+
+int main()
+{
+	const A a(10);
+	const A * p = a.getPointer();
+	cout << p->v << endl;
+	return 0;
+}
