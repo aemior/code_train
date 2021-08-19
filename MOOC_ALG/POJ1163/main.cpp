f759463bb4e8f18c6e8bab6ba3a089c6c0bbecf9
snowshow4@gmail.com <snowshow4@gmail.com>
2021-08-19 19:07:44 +0800

    new ac

--- MOOC_ALG/POJ1163/main.cpp
+++ MOOC_ALG/POJ1163/main.cpp
@@ -1,4 +1,3 @@
-//#include <bits/stdc++.h>
 #include <iostream>
 
 using namespace std;
@@ -8,17 +7,9 @@ using namespace std;
 int N;
 
 int DATA[MAX_NUM+10][MAX_NUM+10];
-int MS[MAX_NUM+10][MAX_NUM+10];
-
-int MaxSum(int i, int j) {
-	if (i >= N-1) return DATA[i][j];
-	int A = MaxSum(i+1, j);
-	int B = MaxSum(i+1, j+1);
-	return A>B?DATA[i][j]+A:DATA[i][j]+B;
-}
+int MS[2][MAX_NUM+10];
 
 int main () {
-	//freopen("test.txt", "r", stdin);
 	cin >> N;
 	for (int i=0; i<N; ++i) {
 		for (int j=0; j<i+1; ++j) {
@@ -26,13 +17,12 @@ int main () {
 		}
 	}
 	for (int j=0; j<N-1; ++j)
-		MS[N-1][j] = DATA[N-1][j];
+		MS[(N-1)%2][j] = DATA[N-1][j];
 	for (int i=N-2; i>=0; --i) {
 		for (int j=0; j < i+1; ++j) {
-			MS[i][j] = DATA[i][j] + (MS[i+1][j]>MS[i+1][j+1]?MS[i+1][j]:MS[i+1][j+1]);
+			MS[i%2][j] = DATA[i][j] + (MS[(i+1)%2][j]>MS[(i+1)%2][j+1]?MS[(i+1)%2][j]:MS[(i+1)%2][j+1]);
 		}
 	}
-	//cout << MaxSum(0,0) << endl;
 	cout << MS[0][0];
 	return 0;
 }
---
+++ MOOC_ALG/POJ2533/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int N;
+int DATA[1001];
+int CHAIN[1001];
+int MAX[1001];
+
+int main () {
+	freopen("test.txt", "r", stdin);
+	cin >> N;
+	for (int i=0; i<N; ++i) {
+		cin >> DATA[i];
+	}
+	for (int i=0; i<N; ++i) {
+		MAX[i] = 1;
+		CHAIN[i] = 1;
+		for (int j=0; j<i; ++j) {
+			if (MAX[j] > MAX[i]) MAX[i] = MAX[j];
+			if (DATA[j] < DATA[i] && CHAIN[j]+1 > CHAIN[i])
+				CHAIN[i] = CHAIN[j] + 1;
+		}
+		if (CHAIN[i] > MAX[i]) MAX[i] = CHAIN[i];
+	}
+	cout << MAX[N-1];
+}
